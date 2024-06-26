d578f8f361429b523917d7d5bd341f555a66f257
Vu Hoang Tien <vht.20021020@gmail.com>
2024-06-26 17:09:52 +0700

    feat: create layout

--- nextjs-tienvh/src/components/user/LoginForm.tsx
+++ nextjs-tienvh/src/components/user/LoginForm.tsx
@@ -1,9 +1,109 @@
-import React from 'react'
+'use client';
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+import TextField from '@mui/material/TextField';
+import Typography from '@mui/material/Typography';
+import Link from 'next/link';
+import { useRouter } from 'next/navigation';
+import { Container } from '@mui/material';
+import { useState } from 'react'
+import { useForm } from 'react-hook-form';
 
-function LoginForm() {
+interface LoginFormData {
+  email: string,
+  password: string
+}
+export default  function LoginForm() {
+  const [isSubmitting, setIsSubmitting] = useState(false);
+  const router = useRouter();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isValid },
+  } = useForm<LoginFormData>({
+    defaultValues: {
+      email: "",
+      password: ""
+    },
+    mode: "onChange",
+  });
+
   return (
-    <div>LoginForm</div>
-  )
+    <Container
+      sx={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        minHeight: "100vh",
+        padding: '20px'
+      }}
+    >
+      <Box component="form" sx={{display: 'flex', justifyContent: 'center', flexDirection: 'column', gap: 3, padding: '20px', boxSizing: 'border-box'}}>
+        <Typography variant="h4" sx={{ fontWeight:'bold'}} mb={3}>
+          Đăng nhập vào tài khoản
+        </Typography>
+
+        <Box sx={{display: 'flex', flexDirection: 'column'}}>
+          <label style={{paddingBottom: '5px'}}>Email</label>
+          <TextField
+            type="email"
+            placeholder="Nhập email của bạn"
+          />
+        </Box>
+
+        <Box sx={{display: 'flex', flexDirection: 'column'}}>
+          <label style={{paddingBottom: '5px'}}>Mật khẩu</label>
+          <TextField
+            type="password"
+            placeholder="Nhập mật khẩu của bạn"
+          />
+        </Box>
+        
+        <Link className="forgot-pass" href="/user/forgot-password">Quên mật khẩu?</Link>
+
+        <Button
+          type="submit"
+          variant="contained"
+          fullWidth
+          disabled={isSubmitting}
+          sx={{
+            fontWeight: 'bold',
+            fontSize: '16px',
+            backgroundColor: "#66bb6a",
+            height: '50px',
+            "&:hover": {
+              backgroundColor: "#66bb6a",
+              opacity: 0.9,
+            },
+          }}
+        >
+          {isSubmitting ? 'Đang đăng nhập...' : 'Đăng nhập'}
+        </Button>
+
+        <Typography sx={{textAlign: 'center'}}>
+          Hoặc đăng ký tài khoản, nếu bạn chưa đăng ký!
+        </Typography>
+
+        <Button
+          variant="outlined"
+          fullWidth
+          sx={{
+            fontWeight: 'bold',
+            fontSize: '16px',
+            color: '#9ccc65',
+            borderColor: "#9ccc65",
+            height: '50px',
+            marginTop: '100px',
+            "&:hover": {
+              opacity: 0.9,
+            },
+          }}
+        >
+          Đăng ký
+        </Button>
+      </Box>
+    </Container>
+  );
 }
 
-export default LoginForm
+
--- nextjs-tienvh/src/lib/theme.ts
+++ nextjs-tienvh/src/lib/theme.ts
@@ -13,16 +13,62 @@ const theme = createTheme({
       'sans-serif',
     ].join(','),
     h1: {
-      fontSize: '2.5rem',
-      fontWeight: 500,
+      fontSize: '6rem',
+      fontWeight: 'light',
+      letterSpacing: '-0.6px'
     },
     h2: {
-      fontSize: '2rem',
-      fontWeight: 500,
+      fontSize: '3.75rem',
+      fontWeight: 'light',
+      letterSpacing: '-0.6px',
+      textTransform: 'uppercase'
+    },
+    h3: {
+      fontSize: '3rem',
+      letterSpacing: '-0.2px',
+      textTransform: 'uppercase'
+    },
+    h4: {
+      fontSize: '2.125rem',
+      textTransform: 'uppercase'
+    },
+    h5: {
+      fontSize: '1.5rem',
+      textTransform: 'uppercase'
+    },
+    h6: {
+      fontSize: '1.25rem',
+      fontWeight: 'medium',
+      letterSpacing: '-0.05px',
+      textTransform: 'uppercase'
     },
     body1: {
       fontSize: '1rem',
+      letterSpacing:'-0.04px'
     },
+    body2: {
+      fontSize: '0.875rem',
+      letterSpacing:'-0.04px'
+    },
+    subtitle1: {
+      fontSize: '1rem',
+      letterSpacing:'-0.04px'
+    },
+    subtitle2: {
+      fontSize: '0.875rem',
+      letterSpacing:'-0.05px',
+      fontWeight:'medium'
+    },
+    caption: {
+      fontSize: '0.75rem',
+      letterSpacing:'0.33px'
+    },
+    overline: {
+      fontSize: '0.75rem',
+      letterSpacing:'-0.04px',
+      textTransform: 'uppercase'
+    }
+
   },
   components: {
     MuiButton: {
