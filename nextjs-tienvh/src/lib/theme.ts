eaa5828d4a7275bed1edb099c4c1c0e0eda4cf30
Vu Hoang Tien <vht.20021020@gmail.com>
2024-06-26 10:41:31 +0700

    Add .editorconfig and setup extentions

--- nextjs-tienvh/src/lib/theme.ts
+++ nextjs-tienvh/src/lib/theme.ts
@@ -2,22 +2,6 @@
 import { createTheme } from '@mui/material/styles';
 
 const theme = createTheme({
-  // palette: {
-  //   primary: {
-  //     main: '#1976d2', 
-  //     light: '#42a5f5',
-  //     dark: '#1565c0',
-  //   },
-  //   secondary: {
-  //     main: '#9c27b0',
-  //     light: '#ba68c8',
-  //     dark: '#7b1fa2',
-  //   },
-  //   background: {
-  //     default: '#f5f5f5',
-  //     paper: '#ffffff',
-  //   },
-  // },
   typography: {
     fontFamily: [
       'Roboto',
@@ -49,15 +33,6 @@ const theme = createTheme({
         },
       },
     },
-    // MuiTextField: {
-    //   styleOverrides: {
-    //     root: {
-    //       '& .MuiOutlinedInput-root': {
-    //         borderRadius: 8,
-    //       },
-    //     },
-    //   },
-    // },
   },
 });
 
