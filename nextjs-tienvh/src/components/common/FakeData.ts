a660148cda34e90664f96d7803decd4f30e23d42
Vu Hoang Tien <vht.20021020@gmail.com>
2024-07-10 13:35:19 +0700

    fix: update fake data

--- nextjs-tienvh/src/components/common/FakeData.ts
+++ nextjs-tienvh/src/components/common/FakeData.ts
@@ -20,3 +20,76 @@ export interface UserInfo {
   district: number;
   ward: number;
 }
+
+export type Ward = {
+  id: number;
+  name: string;
+};
+export type District = {
+  id: number;
+  name: string;
+  provinceId: number;
+  wards: Ward[];
+};
+export type Province = {
+  id: number;
+  name: string;
+  districts: District[];
+};
+
+type LocationData = Province[];
+const locationData: LocationData = [
+  {
+    id: 1,
+    name: "Hà Nội",
+    districts: [
+      {
+        id: 1,
+        name: "Ba Đình",
+        provinceId: 1,
+        wards: [
+          { id: 1, name: "Phúc Xá" },
+          { id: 2, name: "Trúc Bạch" },
+          { id: 3, name: "Vĩnh Phúc" },
+        ],
+      },
+      {
+        id: 2,
+        name: "Cầu Giấy",
+        provinceId: 1,
+        wards: [
+          { id: 1, name: "Nghĩa Đô" },
+          { id: 2, name: "Nghĩa Tân" },
+          { id: 3, name: "Mai Dịch" },
+        ],
+      },
+    ],
+  },
+  {
+    id: 2,
+    name: "Hồ Chí Minh",
+    districts: [
+      {
+        id: 1,
+        name: "Quận 1",
+        provinceId: 2,
+        wards: [
+          { id: 1, name: "Bến Nghé" },
+          { id: 2, name: "Bến Thành" },
+          { id: 3, name: "Cầu Kho" },
+        ],
+      },
+      {
+        id: 2,
+        name: "Quận 2",
+        provinceId: 2,
+        wards: [
+          { id: 1, name: "An Phú" },
+          { id: 2, name: "Thảo Điền" },
+          { id: 3, name: "Bình An" },
+        ],
+      },
+    ],
+  },
+];
+export default locationData
--- nextjs-tienvh/src/components/common/SearchItem.tsx
+++ nextjs-tienvh/src/components/common/SearchItem.tsx
@@ -4,6 +4,8 @@ import { useForm, Controller, useWatch } from 'react-hook-form';
 import SearchIcon from '@mui/icons-material/Search';
 import { styled } from '@mui/material/styles';
 import { tableCellClasses } from '@mui/material/TableCell';
+import locationData, { District, Ward } from './FakeData';
+
 
 const StyledTableCell = styled(TableCell)(({ theme }) => ({
   [`&.${tableCellClasses.head}`]: {
@@ -24,77 +26,6 @@ const StyledTableRow = styled(TableRow)(({ theme }) => ({
     border: 0,
   },
 }));
-type Ward = {
-  id: number;
-  name: string;
-};
-type District = {
-  id: number;
-  name: string;
-  provinceId: number;
-  wards: Ward[];
-};
-type Province = {
-  id: number;
-  name: string;
-  districts: District[];
-};
-
-type LocationData = Province[];
-const locationData: LocationData = [
-  {
-    id: 1,
-    name: "Hà Nội",
-    districts: [
-      {
-        id: 1,
-        name: "Ba Đình",
-        provinceId: 1,
-        wards: [
-          { id: 1, name: "Phúc Xá" },
-          { id: 2, name: "Trúc Bạch" },
-          { id: 3, name: "Vĩnh Phúc" },
-        ],
-      },
-      {
-        id: 2,
-        name: "Cầu Giấy",
-        provinceId: 1,
-        wards: [
-          { id: 1, name: "Nghĩa Đô" },
-          { id: 2, name: "Nghĩa Tân" },
-          { id: 3, name: "Mai Dịch" },
-        ],
-      },
-    ],
-  },
-  {
-    id: 2,
-    name: "Hồ Chí Minh",
-    districts: [
-      {
-        id: 1,
-        name: "Quận 1",
-        provinceId: 2,
-        wards: [
-          { id: 1, name: "Bến Nghé" },
-          { id: 2, name: "Bến Thành" },
-          { id: 3, name: "Cầu Kho" },
-        ],
-      },
-      {
-        id: 2,
-        name: "Quận 2",
-        provinceId: 2,
-        wards: [
-          { id: 1, name: "An Phú" },
-          { id: 2, name: "Thảo Điền" },
-          { id: 3, name: "Bình An" },
-        ],
-      },
-    ],
-  },
-];
 
 //Fake data
 const generateFakeData = (count: number): VaccinationPoint[] => {
