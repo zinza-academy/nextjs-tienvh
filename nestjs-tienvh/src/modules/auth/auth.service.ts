8a7e0158f4c5faeda880de89ba32243b31ade9e9
Vu Hoang Tien <vht.20021020@gmail.com>
2024-07-24 13:52:14 +0700

    feat(implement_user_registration_api): add register api

--- nestjs-tienvh/src/modules/auth/auth.service.ts
+++ nestjs-tienvh/src/modules/auth/auth.service.ts
@@ -3,6 +3,9 @@ import { JwtService } from '@nestjs/jwt';
 import * as bcrypt from 'bcrypt';
 import { UsersService } from '../users/users.service';
 import { LoginDTO } from './dto/login.dto';
+import { RegisterDTO } from './dto/register.dto';
+import { ApiResponse } from 'common/utils/response.util';
+import { ReceiveUserDto } from 'modules/users/dto/users.dto';
 
 @Injectable()
 export class AuthService {
@@ -20,5 +23,9 @@ export class AuthService {
     const payload = { id: user.id, name: user.name, email: user.email, role: user.role };
     const accessToken = this.jwtService.sign(payload);
     return { access_token: accessToken };
+  }
+
+  async register(registerDto: RegisterDTO): Promise<ApiResponse<ReceiveUserDto>>{
+    return this.usersService.create(registerDto);
   }
 }
