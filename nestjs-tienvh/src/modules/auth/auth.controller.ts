fba54ce660b2ae09f0058c9efd608b6580471e8c
Vũ Hoàng Tiến <116150611+tienhvu@users.noreply.github.com>
2024-07-26 14:47:05 +0700

    feat(implement_user_registration_api): add register api (#30)

--- nestjs-tienvh/src/modules/auth/auth.controller.ts
+++ nestjs-tienvh/src/modules/auth/auth.controller.ts
@@ -1,16 +1,18 @@
-  import { Body, Controller, HttpStatus, Post, Res } from '@nestjs/common';
-import { createResponse } from 'common/utils/response.util';
+  import { Body, Controller, HttpStatus, Post, Res, ValidationPipe } from '@nestjs/common';
+import { ApiResponse, createResponse } from 'common/utils/response.util';
 import { Response } from 'express';
 import { AuthService } from './auth.service';
 import { Public } from './decorators/public-route.decorator';
 import { LoginDTO } from './dto/login.dto';
+import { RegisterDTO } from './dto/register.dto';
+import { ReceiveUserDto } from 'modules/users/dto/users.dto';
   @Controller('auth')
   export class AuthController {
     constructor(private authService: AuthService) {}
 
     @Public()
     @Post('login')
-    async login(@Body() loginDto: LoginDTO, @Res({ passthrough: true }) res: Response) {
+    async login(@Body(ValidationPipe) loginDto: LoginDTO, @Res({ passthrough: true }) res: Response) {
       const { access_token } = await this.authService.login(loginDto);
       
       res.cookie('access_token', access_token, {
@@ -27,5 +29,12 @@ import { LoginDTO } from './dto/login.dto';
     async logOut(@Res({ passthrough: true }) res: Response) {
       res.clearCookie('access_token');
       return createResponse(null, 'Logout successful', HttpStatus.OK);
+    }
+
+    @Public()
+    @Post('register')
+    async register(@Body(ValidationPipe) registerDto: RegisterDTO): Promise<ApiResponse<ReceiveUserDto>> {
+      const result = await this.authService.register(registerDto);
+      return createResponse(result.data, 'Registration successful', HttpStatus.CREATED);
     }
   }
